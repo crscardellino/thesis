e41f8e3ced3f9a02864f119f194e8ac49d674929
Cristian Cardellino <crscardellino@gmail.com>
2017-02-02 19:03:20 -0300

    Sensem reparse now takes multiple sentences to give them to freeling

--- thesis/scripts/sensem_reparse.py
+++ thesis/scripts/sensem_reparse.py
@@ -30,38 +30,51 @@ if __name__ == '__main__':
                                 'short_tag', 'morpho_info', 'nec', 'sense', 'syntax',
                                 'dephead', 'deprel', 'coref', 'srl')
 
+    tokenized_freeling_sentences = []
+    sentences = []
+
     for sidx, sentence in enumerate(tqdm(parser.sentences, total=args.sentences), start=1):
         tokenized_sentence = [word.token for word in sentence]
         tokenized_sentence = [token for widx, token in enumerate(tokenized_sentence)
                               if widx == 0 or (widx > 0 and tokenized_sentence[widx-1].lower() != token.lower())]
-        tokenized_sentence = '\n'.join(tokenized_sentence) + '\n\n'
+        tokenized_sentence = '\n'.join(tokenized_sentence)
 
-        freeling_args = (
-            '-f', 'es.cfg',
-            '--input', 'freeling',
-            '--inplv', 'splitted',
-            '--output', 'conll',
-            '--outlv', 'dep',
-            '--ner', '--nec', '--loc',
-            '--dep', 'treeler'
-        )
+        tokenized_freeling_sentences.append(tokenized_sentence)
+        sentences.append(sentence)
 
-        parsed_sentence = sh.analyze(*freeling_args, _in=tokenized_sentence)
-        parsed_sentence = sh.awk('{ print $1, $2, $3, $4, $5, $6, $7, $10, $11 }', _in=parsed_sentence)
+        if (sidx % 100 == 0) or (sidx == args.sentences):
+            tokenized_freeling_sentences = '\n\n'.join(tokenized_freeling_sentences) + '\n\n'
 
-        parsed_sentence = parsed_sentence.strip().split('\n')
-        parsed_sentence_lemma = parsed_sentence[sentence.verb_position-1].strip().split()[2]
+            freeling_args = (
+                '-f', 'es.cfg',
+                '--input', 'freeling',
+                '--inplv', 'splitted',
+                '--output', 'conll',
+                '--outlv', 'dep',
+                '--ner', '--nec', '--loc',
+                '--dep', 'treeler'
+            )
 
-        if parsed_sentence_lemma != sentence.verb_lemma:
-            tqdm.write('NOT FOUND LEMMA for sentence %s (%s != %s)'
-                       % (sentence.sentence_index, sentence.verb_lemma, parsed_sentence_lemma))
-            sentence['verb_position'] = '-'
+            parsed_sentences = sh.analyze(*freeling_args, _in=tokenized_freeling_sentences)
+            parsed_sentences = sh.awk('{ print $1, $2, $3, $4, $5, $6, $7, $10, $11 }', _in=parsed_sentences)
 
-        printing_sentence = '\n'.join(parsed_sentence)
-        printing_sentence = sh.column('-t', _in=printing_sentence.strip() + '\n')
+            for sentence, parsed_sentence in zip(sentences, parsed_sentences.strip().split('\n\n')):
+                parsed_sentence = parsed_sentence.strip().split('\n')
+                parsed_sentence_lemma = parsed_sentence[sentence.verb_position-1].strip().split()[2]
 
-        print(sentence.metadata_string, file=output)
-        print(printing_sentence.strip(), file=output, end='\n\n')
+                if parsed_sentence_lemma != sentence.verb_lemma:
+                    tqdm.write('NOT FOUND LEMMA for sentence %s (%s != %s)'
+                               % (sentence.sentence_index, sentence.verb_lemma, parsed_sentence_lemma))
+                    sentence['verb_position'] = '-'
+
+                printing_sentence = '\n'.join(parsed_sentence)
+                printing_sentence = sh.column('-t', _in=printing_sentence.strip() + '\n')
+
+                print(sentence.metadata_string, file=output)
+                print(printing_sentence.strip(), file=output, end='\n\n')
+
+            parsed_sentences = []
+            sentences = []
 
     if args.output is not None:
         output.close()

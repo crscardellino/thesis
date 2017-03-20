6046fa2c13a0338a92b81e227566149b686c6a44
Cristian Cardellino <crscardellino@gmail.com>
2017-03-20 17:33:18 -0300

    Added some stuff to gitignore

--- helpers/run_classifiers.sh
+++ helpers/run_classifiers.sh
@@ -3,39 +3,73 @@ set -e
 
 cd ..
 
-find ../resources -type d -not -name "resources" | sort | while read directory
+find ../resources -type d -name "word_window_sensem" | sort | while read directory
 do
     dataset=$(basename $directory)
 
-    for classifier in baseline decision_tree log svm
+    for wv in sbwcesampled
     do
-        >&2 echo "Running classifier $classifier for $dataset"
+        >&2 echo "Running classifier svm for $dataset"
         python -m thesis.classification \
             $directory/train_dataset.npz \
             $directory/test_dataset.npz \
-            ../results/${classifier}_${dataset}.csv \
-            --classifier $classifier \
-            --max_features 10000
+            ../results/experiment_word_vectors/svm_${dataset}_${wv}.csv \
+            --classifier svm \
+            --word_vectors_model_path ../resources/word_vectors/${wv}.wordvectors.bin.gz
+
+        >&2 echo "Running classifier mlp for $dataset"
+        python -m thesis.classification \
+            $directory/train_dataset.npz \
+            $directory/test_dataset.npz \
+            ../results/experiment_word_vectors/mlp_1800_${dataset}_${wv}.csv \
+            --classifier mlp \
+            --word_vectors_model_path ../resources/word_vectors/${wv}.wordvectors.bin.gz \
+            --layers 1800
+
+        for splits in 3
+        do
+            for folds in 3 10
+            do
+                if [[ $(( splits * folds )) -gt 30 ]]
+                then
+                    continue
+                fi
+
+                >&2 echo "Running classifier svm for $dataset for $splits splits and $folds folds"
+                python -m thesis.classification \
+                    $directory/train_dataset.npz \
+                    $directory/test_dataset.npz \
+                    ../results/experiment_word_vectors/svm_${dataset}_${splits}_splits_${folds}_folds_${wv}.csv \
+                    --classifier svm \
+                    --word_vectors_model_path ../resources/word_vectors/${wv}.wordvectors.bin.gz \
+                    --splits $splits \
+                    --folds $folds \
+                    --ensure_minimum
+
+                >&2 echo "Running classifier mlp for $dataset for $splits splits and $folds folds"
+                python -m thesis.classification \
+                    $directory/train_dataset.npz \
+                    $directory/test_dataset.npz \
+                    ../results/experiment_word_vectors/mlp_1800_${dataset}_${splits}_splits_${folds}_folds_${wv}.csv \
+                    --classifier mlp \
+                    --word_vectors_model_path ../resources/word_vectors/${wv}.wordvectors.bin.gz \
+                    --layers 1800 \
+                    --splits $splits \
+                    --folds $folds \
+                    --ensure_minimum
+
+                >&2 echo "Running classifier mlp with 2 layers for $dataset for $splits splits and $folds folds"
+                python -m thesis.classification \
+                    $directory/train_dataset.npz \
+                    $directory/test_dataset.npz \
+                    ../results/experiment_word_vectors/mlp_1800_900_${dataset}_${splits}_splits_${folds}_folds_${wv}.csv \
+                    --classifier mlp \
+                    --word_vectors_model_path ../resources/word_vectors/${wv}.wordvectors.bin.gz \
+                    --layers 1800 900 \
+                    --splits $splits \
+                    --folds $folds \
+                    --ensure_minimum
+            done
+        done
     done
-
-    # We run the non negative datasets for naive bayes
-    if ! [[ $dataset =~ "negative" ]]
-    then
-        >&2 echo "Running classifier naive_bayes for $dataset"
-        python -m thesis.classification \
-            $directory/train_dataset.npz \
-            $directory/test_dataset.npz \
-            ../results/naive_bayes_${dataset}.csv \
-            --classifier naive_bayes \
-            --max_features 10000
-    fi
-
-    >&2 echo "Running classifier mlp for $dataset"
-    python -m thesis.classification \
-        $directory/train_dataset.npz \
-        $directory/test_dataset.npz \
-        ../results/mlp_5000_${dataset}.csv \
-        --classifier mlp \
-        --max_features 10000 \
-        --layers 5000
 done

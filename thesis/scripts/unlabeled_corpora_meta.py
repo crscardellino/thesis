35ea0f6a91d185b2d7e5d0443790d8dfd9b17164
Cristian Cardellino <crscardellino@gmail.com>
2017-05-17 09:56:50 -0300

    Changed unlabeled corpora meta to support multiprocessing

--- thesis/scripts/unlabeled_corpora_meta.py
+++ thesis/scripts/unlabeled_corpora_meta.py
@@ -5,9 +5,31 @@ from __future__ import absolute_import, print_function, unicode_literals
 import argparse
 import os
 
+from functools import partial
+from multiprocessing import Pool
 from tabulate import tabulate
 from thesis.utils import find
-from tqdm import tqdm
+
+
+def process_file(ifile, meta):
+    basename = os.path.basename(ifile)
+    ofile = os.path.join(args.output, basename)
+    with open(ifile, 'r') as fin, open(ofile, 'w') as fout:
+        sentence = []
+        sentences = 0
+
+        for line in fin:
+            line = line.strip().split()
+
+            if not line and sentence:
+                print('META:%s    sentence:%05d    file:%s    words:%03d'
+                      % (meta, sentences, basename, len(sentence)), file=fout)
+                print(tabulate(sentence, tablefmt='plain'), end='\n\n', file=fout)
+                sentence = []
+                sentences += 1
+            elif line:
+                sentence.append(line)
+
 
 
 if __name__ == '__main__':
@@ -16,26 +38,10 @@ if __name__ == '__main__':
     parser.add_argument('output')
     parser.add_argument('meta')
     parser.add_argument('--pattern', default='*')
+    parser.add_argument('--workers', type=int, default=12)
 
     args = parser.parse_args()
 
-    files = list(find(args.input, args.pattern))
+    with Pool(args.workers) as p:
+        p.map(partial(process_file, meta=args.meta), find(args.input, args.pattern))
 
-    for ifile in tqdm(files):
-        basename = os.path.basename(ifile)
-        ofile = os.path.join(args.output, basename)
-        with open(ifile, 'r') as fin, open(ofile, 'w') as fout:
-            sentence = []
-            sentences = 0
-
-            for line in ifile:
-                line = line.strip().split()
-
-                if not line and sentence:
-                    print('META:%s    sentence:%05d    file:%s    words:%03d'
-                          % (args.meta, sentences, basename, len(sentence)))
-                    print(tabulate(sentence, tablefmt='plain'), end='\n\n', file=fout)
-                    sentence = []
-                    sentences += 1
-                elif line:
-                    sentence.append(line)

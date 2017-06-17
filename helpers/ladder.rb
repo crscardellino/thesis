e6ccc03eb5fa7656397aa5443e7881b051fbef30
Cristian Cardellino <crscardellino@gmail.com>
2017-06-17 03:24:48 -0300

    Added info about noise and epochs

--- helpers/ladder.rb
+++ helpers/ladder.rb
@@ -5,6 +5,8 @@ Dir.chdir '..'
 REPRESENTATIONS = %w(hashed word_window)
 CORPORA = %w(sensem)
 LAYERS = %w(100 250_100 100_100_100 250_100_100 250_250_100 500_250_100 500_250_250_100)
+EPOCHS = %w(25 50)
+NOISE = %w(0.2 0.3 0.5)
 VECTOR_DOMAIN = [nil] + %w(journal)
 ACCEPTANCE_THRESHOLD = 0.9
 VALIDATION_RATIO = 0.2
@@ -22,18 +24,22 @@ BASE_CMD.concat("--error_sigma #{ERROR_SIGMA} ")
 BASE_CMD.concat("--unlabeled_data_limit #{UNLABELED_DATA_LIMIT} ")
 BASE_CMD.concat("--validation_ratio #{VALIDATION_RATIO} ")
 BASE_CMD.concat('--min_count 2 ')
+BASE_CMD.concat('--epochs %{epochs} ')
+BASE_CMD.concat('--noise_std %{noise} ')
 BASE_CMD.concat('--corpus_name %{corpus} ')
 BASE_CMD.concat('--representation %{representation} ')
 
-BASE_RESULTS = '%{corpus}_%{representation}_%{layers}_%{domain}'
+BASE_RESULTS = '%{corpus}_%{representation}_%{layers}_%{domain}_%{epochs}_%{noise}'
 
-def generate_command(representation, corpus, language, domain=nil, layers=nil)
+def generate_command(representation, corpus, language, layers, epochs, noise, domain=nil)
   cmd = BASE_CMD
-  cmd_hash = {representation: representation, corpus: corpus, language: language}
+  cmd_hash = {representation: representation, corpus: corpus, language: language, epochs: epochs, noise: noise}
   results = BASE_RESULTS
   results_hash = Hash.new('NA')
   results_hash[:corpus] = corpus
   results_hash[:representation] = representation
+  results_hash[:epochs] = epochs
+  results_hash[:noise] = noise
 
   if domain != nil
     cmd += '--word_vector_model_path ../resources/word_vectors/%{domain}.bin.gz '
@@ -57,13 +63,13 @@ def generate_command(representation, corpus, language, domain=nil, layers=nil)
   cmd % cmd_hash
 end
 
-REPRESENTATIONS.product(CORPORA, VECTOR_DOMAIN, LAYERS).each do
-  |representation, corpus, domain, layers|
+REPRESENTATIONS.product(CORPORA, VECTOR_DOMAIN, LAYERS, EPOCHS, NOISE).each do
+  |representation, corpus, domain, layers, epochs, noise|
 
   next if (representation == 'word_window' and not domain) or (representation != 'word_window' and domain)
 
-  cmd = generate_command(representation, corpus, LANGUAGE[corpus.to_sym], domain, layers)
+  cmd = generate_command(representation, corpus, LANGUAGE[corpus.to_sym], layers, epochs, noise, domain)
   STDERR.puts cmd
-  raise 'Error on command' unless system cmd
+  #raise 'Error on command' unless system cmd
 end
 
